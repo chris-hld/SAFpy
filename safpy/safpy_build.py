e22ed6362ed41be47d2cabae4484baa82efa9eee
Chris Hold <Christoph.Hold@aalto.fi>
2020-08-04 18:04:06 +0300

    Add getSHcomplex

--- safpy/safpy_build.py
+++ safpy/safpy_build.py
@@ -4,13 +4,26 @@ ffibuilder = FFI()
 # cdef() expects a single string declaring the C types, functions and
 # globals needed to use the shared object. It must be in valid C syntax.
 ffibuilder.cdef("""
+
+    typedef float _Complex float_complex;
+    typedef double _Complex double_complex;
+
     long double factorial(int n);
+
     void getSHreal(/* Input Arguments */
                int order,
                float* dirs_rad,
                int nDirs,
                /* Output Arguments */
                float* Y);
+
+    void getSHcomplex(/* Input Arguments */
+                      int order,
+                      float* dirs_rad,
+                      int nDirs,
+                      /* Output Arguments */
+                      float_complex* Y);
+
 """)
 
 # set_source() gives the name of the python extension module to
--- safpy/wrapit.py
+++ safpy/wrapit.py
@@ -1,7 +1,55 @@
 """This will have the Python functions."""
 import sys
+from ._safpy import ffi, lib
+
+
+import numpy as np
 
 
 def whoami():
     """Pure Python function."""
-    print (sys.version)
+    print(sys.version)
+
+
+def factorial(n):
+    return np.longdouble(lib.factorial(n))
+
+
+def getSHreal(order, dirs_deg):
+    """
+    
+
+    Parameters
+    ----------
+    order : TYPE
+        DESCRIPTION.
+    dirs_deg : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    Y : TYPE
+        DESCRIPTION.
+
+    """
+    order = int(order)
+    dirs_deg = np.atleast_2d(dirs_deg)
+    dirs_deg = np.ascontiguousarray(dirs_deg, dtype=np.float32)
+    nDirs = np.shape(dirs_deg)[0]
+    Y = np.zeros(((order+1)**2, nDirs), dtype=np.float32)
+    Y_ptr = ffi.cast("float *", ffi.from_buffer(Y))  # out pointer
+    lib.getSHreal(order, ffi.cast("float *", ffi.from_buffer(dirs_deg)),
+                  nDirs, Y_ptr)
+    return Y
+
+
+def getSHcomplex(order, dirs_deg):
+    order = int(order)
+    dirs_deg = np.atleast_2d(dirs_deg)
+    dirs_deg = np.ascontiguousarray(dirs_deg, dtype=np.float32)
+    nDirs = np.shape(dirs_deg)[0]
+    Y = np.zeros(((order+1)**2, nDirs), dtype=np.complex64)
+    Y_ptr = ffi.cast("float_complex *", ffi.from_buffer(Y))  # out pointer
+    lib.getSHcomplex(order, ffi.cast("float *", ffi.from_buffer(dirs_deg)),
+                     nDirs, Y_ptr)
+    return Y

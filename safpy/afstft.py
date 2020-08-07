5a6187ff161385247fd30569da1c490e0024ac0e
Chris Hold <christoph.hold@aalto.fi>
2020-08-07 19:31:37 +0300

    Start AFSTFT

--- safpy/afstft.py
+++ safpy/afstft.py
@@ -3,9 +3,14 @@ import numpy as np
 from ._safpy import ffi, lib
 
 
-def AfSTFT():
+class AfSTFT():
     def __init__(self, num_ch_in, num_ch_out, hopsize, HYBRIDMODE=True,
                  LOWDELAYMODE=False):
         # need to call constructor of afstft
-        afstft_format = ffi.new("AFSTFT_FDDATA_FORMAT", 0)
+        afstft_format = 0
+        # let the compiler fill in the rest
+        handle_ptr = ffi.new("void ** const")
+        lib.afSTFT_create(handle_ptr,
+                          num_ch_in, num_ch_out, hopsize,
+                          LOWDELAYMODE, HYBRIDMODE, afstft_format)
 
--- test_call.py
+++ test_call.py
@@ -8,3 +8,4 @@ print(safpy.sh.getSHreal(1, [0, 0]))
 print(safpy.sh.getSHcomplex(1, [0, 0]))
 print(safpy.vbap.generateVBAPgainTable3D(np.reshape(np.arange(10), (5, 2)),
                                            1, 1, 0, 0, 0))
+h = safpy.afstft.AfSTFT(2, 2, 128)

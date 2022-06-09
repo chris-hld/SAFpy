075fc9c098be7a189bd11a6cac75a265c0af57fa
Chris Hold <Christoph.Hold@aalto.fi>
2022-06-09 14:39:05 +0300

    Create utils.py

---
+++ safpy/utils.py
@@ -0,0 +1,17 @@
+"""This will have the Python functions."""
+import sys
+
+import numpy as np
+
+from ._safpy import ffi, lib
+
+
+def whoami():
+    """Printing python and SAF lib version."""
+    print("Python version: ", sys.version)
+    print("SAF lib version: ", lib.SAF_VERSION)
+
+
+def factorial(n):
+    """Compute the factorial of n."""
+    return np.longdouble(lib.factorial(n))
